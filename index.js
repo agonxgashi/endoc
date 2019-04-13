fac3d2a5c150e1918b8f7e7c3b62be77005debfa
Agon G <agonxgashi@gmail.com>
2019-04-14 00:10:23 +0200

    node server on https

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const fs = require('fs')
+const https = require('https')
 
 const routeImporter = require('./routes/route-importer')
 const config = require('./env/config.json')
@@ -10,12 +12,18 @@ const config = require('./env/config.json')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'client/dist')))
-app.use(bodyParser.urlencoded({ 'extended': 'true' }))
+app.use(bodyParser.urlencoded({
+  'extended': 'true'
+}))
 app.use(bodyParser.json())
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+}))
 app.use(cors())
 
-mongoose.connect(config.DB.Mongo_ConStr, { useNewUrlParser: true }, (err) => {
+mongoose.connect(config.DB.Mongo_ConStr, {
+  useNewUrlParser: true
+}, (err) => {
   if (err) {
     console.log('Error: Database connection!\nStopping application...')
     process.exit(22)
@@ -29,6 +37,14 @@ mongoose.connect(config.DB.Mongo_ConStr, { useNewUrlParser: true }, (err) => {
     })
 
     const port = config.Port || 3200
-    app.listen(port, () => console.log('App started successfully on port ' + port))
+    https.createServer({
+      key: fs.readFileSync('env/server.key'),
+      cert: fs.readFileSync('env/server.cert')
+    }, app)
+      .listen(3000, function () {
+        console.log('App started successfully on port ' + port)
+      })
+
+    // app.listen(port, () => console.log('App started successfully on port ' + port))
   }
 })
